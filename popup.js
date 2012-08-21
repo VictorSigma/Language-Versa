e9471c676f16fb4142422aee6aafc78b3a3263d0
Luke BrownGold <lbrowngold@lsc.org>
2012-08-21 10:25:02 -0400

    Add more popup information/ functionality

--- popup.js
+++ popup.js
@@ -5,16 +5,32 @@ $(function() {
 	
 	//set saved values
 	$('input[type="checkbox"]').attr('checked', bck.options.enabled);
+	$('.word_count').text(bck.options.words_shown);
+	$('.page_views').text(bck.options.word_exposure);
 
+
+	chrome.tabs.getSelected(null, function(tab) {
+	    $('.exclude_url').val(tab.url);
+	});
+
+
+	//events 
 	$('input[type="checkbox"]').change(function(){
-
 		bck.options.enabled = this.checked;
 		bck.save();
 		chrome.tabs.reload();
-		
-		//chrome.tabs.create({
-			//url: "options.html"
-}		//)
-		
 	});
+
+	$('.options').click(function(){chrome.tabs.create({url: 'options.html'});});
+
+	$('button.add').click(function(){
+		bck.options.excluded_sites.push($(".exclude_url").val());
+		bck.save();
+	});
+
+
+
+
 });
+
+
