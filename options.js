1bbba27c6ee8542ecf232b1f34c8f60b2988fd48
Luke BrownGold <ma@gmail.com>
2012-09-10 00:46:39 -0400

    Active and que gui added

--- options.js
+++ options.js
@@ -5,24 +5,28 @@ var bck = chrome.extension.getBackgroundPage();
 $(function() {
 
 	function markup_exclude_li(value){
-		return '<li> <input type="text" value = "'+value+'" ><a class = "remove-list-item" href = "#" >x</a> </li>';
+		return '<li> <input type="text" value = "'+value+'" ><a data-icon = "x" class = "remove-list-item icon" href = "#" ></a> </li>';
 	}
 	
 	function markup_words_li(term, definition){
-		return '<li class = "icon" data-icon = "\\" > <input  class = "term" type="text" value = "'+term+'" > = <input value = "'+definition+'" class = "definition" type="text" ><a data-icon = "x" class = "icon remove-list-item" href = "#" ></a> </li> ';
+		return '<li class = "icon" data-icon = "\\" > <input  class = "term" type="text" value = "'+term+'" > <span class="transform">&gt;</span> <input value = "'+definition+'" class = "definition" type="text" ><a data-icon = "x" class = "icon remove-list-item" href = "#" ></a> </li> ';
 	}
 
-
+	//Add exclude list markup
 	if (bck.options.excluded_sites != undefined)
 		for (var i = 0; i< bck.options.excluded_sites.length; i++) {
 			$('section#exclude ul').append(markup_exclude_li(bck.options.excluded_sites[i]));
 		}
 	
+	//Add word list
+	$("section#words ul").append('<li data-icon = ")" class = "unsortable icon unmoveable" ><span>Active Words</span></li>');
+	
 	if (bck.options.dictionary != undefined)
 		for (var term in bck.options.dictionary) {
 			$("section#words ul").append(markup_words_li(term, bck.options.dictionary[term]));
 		}
-
+	
+	$("section#words ul").append('<li data-icon = "B" class = "unsortable  icon " ><span>Word Que</span></li>');
 	
 	//events
 	
@@ -40,7 +44,7 @@ $(function() {
 		bck.options.words = [];
 	
 		$("section#words li").each(function(){
-			if ($(this).find(".term").val() != '' ){
+			if ($(this).find(".term").val() != ''  && $(this).find(".term").val() != undefined){
 				bck.options.dictionary[$(this).find(".term").val()] = $(this).find(".definition").val();
 				bck.options.words.push($(this).find(".term").val());
 			}
@@ -62,6 +66,8 @@ $(function() {
 	/////////
 	
 	$( "section#words #sortable" ).sortable({
+		items: "li:not(.unmoveable)",
+
 		update: function(event, ui) { update(); }
 	});
 	
